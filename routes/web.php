6b73ad992b2261baae0f1771dfcbde4d98bd0896
Matthijs Boet <m.boet@chello.nl>
2021-10-12 21:22:04 +0200

    still doing dbs

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/posts/{post}', function (Post $post) {
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
     ]);
